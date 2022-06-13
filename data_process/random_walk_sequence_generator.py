4a8ce6ddbbbc64e6a168060bb5d71947530cf6c3
Ryan <ruihandou@outlook.com>
2022-06-14 00:11:11 +0800

    调试完成

--- data_process/random_walk_sequence_generator.py
+++ data_process/random_walk_sequence_generator.py
@@ -116,8 +116,8 @@ class RandomWalkSequenceGenerator:
                 if count % 10000 == 0:
                     print(str(round(visited.__len__() / self.items_num * 100, 2)) + "% ready")
                 # TODO ：调试用，调试成功后取消
-                # if count == 30000:
-                #     break
+                if count == 30000:
+                    break
                 # TODO ：调试用，调试成功后取消
                 seq_start = -1
                 for id in range(self.items_num):
